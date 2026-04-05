c5d1f440fc05f637d3dfec4b4d8ea671620b1d56
Dmitry Plesunov <dima.plesunov@gmx.de>
2026-04-06 00:44:15 +0200

    Update library

--- src/main/groovy/dev/gxlg/versiont/toolchain/VersiontPlugin.groovy
+++ src/main/groovy/dev/gxlg/versiont/toolchain/VersiontPlugin.groovy
@@ -21,7 +21,7 @@ class VersiontPlugin implements Plugin<Project> {
 
         project.dependencies {
             implementation "net.bytebuddy:byte-buddy:1.18.4"
-            implementation "dev.gxlg:versiont-library:1.0.2"
+            implementation "dev.gxlg:versiont-library:1.1.0"
         }
 
         def generatedSourceDir = project.layout.buildDirectory.dir("generated/sources/versiont/java").get().asFile
